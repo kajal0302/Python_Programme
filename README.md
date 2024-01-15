3d26ea9850be4a51c42e3a4536ae8fd530819674
kajal maurya <mauryakajal245@gmail.com>
2024-01-15 15:41:40 +0530

    add new file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Python_Programme
-Hello :)
+Hello World :)
---
+++ program_1.py
@@ -0,0 +1,4 @@
+value1=int(input("Enter the value of the number1: "))
+value2=int(input("Enter the value of the number2: "))
+addition=value1+value2
+print(addition)
